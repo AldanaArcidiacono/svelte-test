35a7178b2fdb858e4229af8870e18868cf5f7e3c
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2023-02-06 17:13:17 +0100

    ⚡️ Initial try to use svelte

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import CharacterList from "./lib/CharacterList.svelte";
+import Character from "./lib/CharacterList.svelte";
+
+</script>
+
+<main>
+  <CharacterList></CharacterList>
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/CharacterList.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import type { Result } from "../models/characters";
+
+  let characters: Result[] = [];
+
+  onMount(async () => {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    characters = data.results;
+  });
+</script>
+
+<ul>
+  {#each characters as character}
+    <li>
+      <p>{character.name}</p>
+      <img src={character.image} alt={character.name} />
+    </li>
+  {/each}
+</ul>
